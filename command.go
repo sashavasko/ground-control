c1e23b787b804c7ba1c28d17c718a96df62d5f4b
sashavasko <sashavasko@gmail.com>
2026-09-02 12:01:03 -0500

    Implementing the Command - fixing errors

--- command.go
+++ command.go
@@ -1,18 +1,26 @@
-package command
+package main
+
+import "fmt"
 
 type Command struct {
-    SatelliteID string
-    Sequence uint64
-    Payload []byte
+	SatelliteID string
+	Sequence    uint64
+	Payload     []byte
 }
 
-func NewCommand(satelliteID string, sequence uint64, payload []byte)(Command, error){
-    if satelliteID == "" {
-        return Command{}, fmt.error("Satellite ID is empty")
-    }
-    return Command{
-        SatelliteID: satelliteID,
-        Sequence: sequence,
-        Payload: payload
-    }, nil
+func NewCommand(satelliteID string, sequence uint64, payload []byte) (Command, error) {
+	if satelliteID == "" {
+		return Command{}, fmt.Errorf("Satellite ID is empty")
+	}
+	if sequence == 0 {
+		return Command{}, fmt.Errorf("sequence is empty")
+	}
+	if len(payload) == 0 {
+		return Command{}, fmt.Errorf("payload is empty")
+	}
+	return Command{
+		SatelliteID: satelliteID,
+		Sequence:    sequence,
+		Payload:     payload,
+	}, nil
 }
--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
-    command, err := NewCommand()
-    if err == nil {
-        fmt.Printf("created command %s %d \n", command.SatelliteID, command.Sequence)
-    } else {
-        fmt.Printf("Failed to create command %w", err)
-    }
-
+func main() {
+	command, err := NewCommand("SAT-1", 1, []byte("CAPTURE"))
+	if err != nil {
+		fmt.Printf("Failed to create command %v \n", err)
+	}
+	fmt.Printf("created command %s %d \n", command.SatelliteID, command.Sequence)
 }
